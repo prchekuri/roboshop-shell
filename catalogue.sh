10d1bc26856cb5a9d310742b53834e1b7e6dc6f0
Prameela <ch.prameelarani@gmail.com>
2023-01-14 18:13:07 +0530

    Added Roboshop Catalogue Shell Script.

--- catalogue.sh
+++ catalogue.sh
@@ -1,13 +1,14 @@
 script_location=$(pwd)
-set -e
+#set -e (to stop the script where it fails)
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 yum install nodejs -y
 useradd roboshop
 
-mkdir /app
+mkdir -p /app
 
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+rm -rf /app/*
 cd /app
 unzip /tmp/catalogue.zip
 
@@ -19,3 +20,6 @@ cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.serv
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+yum install mongodb-org-shell -y

6b10a0968209bf67ff92a1c116b90e1507728982
Prameela <ch.prameelarani@gmail.com>
2023-01-16 20:29:12 +0530

    Catalogue Shell Script.

--- catalogue.sh
+++ catalogue.sh
@@ -1,13 +1,4 @@
 source common.sh
-log1=/tmp/catalogue.log
-#print_head "Add Application User"
-#id roboshop &>>${LOG}
-#if [ $? -ne 0 ]
-#then
-#  useradd roboshop &>>${LOG}
-#fi
-#status_check
-#status_check $log1
 
 component=catalogue
 

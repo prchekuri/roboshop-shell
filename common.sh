59fbeb5daf8bdeeb5470fb5993ce183fd127f234
Prameela <ch.prameelarani@gmail.com>
2023-01-16 14:17:30 +0530

    Added log variable to Common file.

--- common.sh
+++ common.sh
@@ -1,4 +1,5 @@
 script_location=$(pwd)
+LOG=/tmp/roboshop.log
 
 status_check(){
   if [ $? -eq 0 ]
@@ -6,6 +7,7 @@ status_check(){
       echo -e "\e[1;32m SUCCESS\e[0m"
     else
       echo -e "\e[1;31m FAILURE\e[0m"
+      echo "Refer log file for more information, Log - ${LOG}"
       exit
   fi
 }

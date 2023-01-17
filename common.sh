85f2777a7ab9c157303b437e9575daf990c6729c
Prameela <ch.prameelarani@gmail.com>
2023-01-17 09:03:17 +0530

    MySQL Database & Shipping Shell Script.

--- common.sh
+++ common.sh
@@ -16,20 +16,10 @@ print_head(){
   echo -e "\e[1m $1 \e[0m"
 }
 
-NODEJS(){
-  print_head "Configuring NodeJS repos"
-  #set -e (to stop the script where it gets errors)
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-  status_check
-
-  print_head "Install NodeJS"
-  yum install nodejs -y &>>${LOG}
-  status_check
-
+APP_PREREQ(){
   print_head "Add Application User"
   id roboshop &>>${LOG}
-  if [ $? -ne 0 ]
-  then
+  if [ $? -ne 0 ]; then
     useradd roboshop &>>${LOG}
   fi
   status_check
@@ -50,39 +40,96 @@ NODEJS(){
   unzip /tmp/${component}.zip &>>${LOG}
   status_check
 
+}
+
+SYSTEMD_SETUP(){
+    print_head "Configuring ${component} Service File"
+    cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+    status_check
+
+    print_head "Reload SysyemD"
+    systemctl daemon-reload &>>${LOG}
+    status_check
+
+    print_head "Enable ${component} Service"
+    systemctl enable ${component} &>>${LOG}
+    status_check
+
+    print_head "Start ${component} Service"
+    systemctl start ${component} &>>${LOG}
+    status_check
+
+}
+
+LOAD_SCHEMA(){
+  if [ ${schema_load} == "true" ]; then
+
+    if [ ${schema_type} == "mongo" ]; then
+      print_head "Configuring Mongodb Repo"
+      cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+      status_check
+
+      print_head "Install Mongodb Client"
+      yum install mongodb-org-shell -y &>>${LOG}
+      status_check
+
+      print_head "Load Schema"
+      mongo --host mongodb.roboshop.internal </app/schema/${component}.js &>>${LOG}
+      status_check
+    fi
+
+    if [ ${schema_type} == "mysql" ]; then
+
+      print_head "Install MySQL Client"
+      yum install mysql -y &>>${LOG}
+      status_check
+
+      print_head "Load Schema"
+      mysql -h mysql.roboshop.internal -uroot -p${root_mysql_password} < /app/schema/${component}.sql &>>${LOG}
+      status_check
+    fi
+
+  fi
+}
+
+NODEJS(){
+  print_head "Configuring NodeJS repos"
+  #set -e (to stop the script where it gets errors)
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+  status_check
+
+  print_head "Install NodeJS"
+  yum install nodejs -y &>>${LOG}
+  status_check
+
+  APP_PREREQ
+
   print_head "Installing NodeJS Dependencies"
   cd /app
   npm install &>>${LOG}
   status_check
 
-  print_head "Configuring ${component} Service File"
-  cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+  SYSTEMD_SETUP
+
+  LOAD_SCHEMA
+}
+
+MAVEN(){
+  print_head "Install Maven"
+  yum install maven -y &>>${LOG}
   status_check
 
-  print_head "Reload SysyemD"
-  systemctl daemon-reload &>>${LOG}
+  APP_PREREQ
+
+  print_head "Build a package"
+  mvn clean package  &>>${LOG}
   status_check
 
-  print_head "Enable ${component} Service"
-  systemctl enable ${component} &>>${LOG}
+  print_head "Copy App file to App Location"
+  mv target/${component}-1.0.jar ${component}.jar
   status_check
 
-  print_head "Start ${component} Service"
-  systemctl start ${component} &>>${LOG}
-  status_check
+  SYSTEMD_SETUP
 
-  print_head "Configuring Mongodb Repo"
-  cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
-  status_check
-
-  print_head "Install Mongodb Client"
-  yum install mongodb-org-shell -y &>>${LOG}
-  status_check
-
-  if [ ${schema_load} == "true" ]
-    then
-      print_head "Load Schema"
-      mongo --host mongodb.roboshop.internal </app/schema/${component}.js &>>${LOG}
-      status_check
-  fi
+  LOAD_SCHEMA
 }

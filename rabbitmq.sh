bd4916b26e2d1bd4e7af6ee18e3043e040dc518e
Prameela <ch.prameelarani@gmail.com>
2023-01-17 16:29:49 +0530

    Rabbitmq Shell Script.

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,32 +8,32 @@ fi
 
 print_head "Configuring Erlang YUM Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>${LOG}
-status-check
+status_check
 
 print_head "Configuring Rabbitmq YUM Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
-status-check
+status_check
 
 print_head "Install Erlang and Rabbitmq Server"
 yum install erlang rabbitmq-server -y &>>${LOG}
-status-check
+status_check
 
 print_head "Enable Rabbitmq Server"
 systemctl enable rabbitmq-server &>>${LOG}
-status-check
+status_check
 
 print_head "Start Rabbitmq Server"
 systemctl start rabbitmq-server &>>${LOG}
-status-check
+status_check
 
 print_head "Add Application User"
 rabbitmqctl add_user roboshop ${roboshop_rabbitmq_password} &>>${LOG}
-status-check
+status_check
 
 print_head "Add tags to Application User"
 rabbitmqctl set_user_tags roboshop administrator &>>${LOG}
-status-check
+status_check
 
 print_head "Add permission to Application User"
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
-status-check
+status_check
